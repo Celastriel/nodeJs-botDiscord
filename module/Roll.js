653b38adcbb8dce48d0cba7f15fe05aa09e7f825
Gillastre <jimmymention@gmail.com>
2021-04-03 21:41:05 +0200

    update Soon

--- module/Roll.js
+++ module/Roll.js
@@ -1,3 +1,5 @@
+const sortNumbers = require('sort-numbers');
+
 module.exports = class Roll{
 
     static DICE = [10];
@@ -15,6 +17,7 @@ module.exports = class Roll{
             let tmp = parseInt(Math.floor(Math.random() * Math.floor(this.typeOfDice))+1)
             this.datas.push(tmp);
         }
+        sortNumbers(this.datas)
     }
 
     result(){
@@ -23,60 +26,6 @@ module.exports = class Roll{
             return r;
         },0);
     }
-
-    mise(){
-       /* if(this.datas.reduce( (a,e) => {
-            if(e>4)a++;
-            return a;
-        },0)===this.datas.length-1&&this.result().toString().charAt(1)<5){
-            return Math.floor(this.result()/10)-1
-        }else{
-            return Math.floor(this.result()/10)
-        }*/
-        let mise = 0;
-        let rolls = [...this.datas]; 
-        rolls.sort().reverse(); //tri par ordre decroissant pour maximiser les paires
-        
-        //console.log(rolls);
-
-        for(let i = 0; i < rolls.length; i++){ //on boucle sur le tableau
-            if(rolls[i]){ // si x null on passe
-                if(rolls[i] == 10){ // si c'est 10, c'est une mise à lui seul
-                    ++mise;
-                    rolls[i] = null;
-                    //console.log(i);
-                } else {
-                    for(let y = rolls.length-1; y >=0 ; y--){ // on boucle sur l'inverse du tableau
-                        if(rolls[y]){ //si y null on passe
-                            if(i !== y){ 
-                                if(rolls[i]+rolls[y] >= 10){
-                                    ++mise;
-                                    rolls[i] = null;
-                                    rolls[y] = null;
-                                    //console.log(i + " & " + y);
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        let tmp = 0;
-        for(let i = 0; i < rolls.length; i++){
-            if(rolls[i]){
-                tmp+=rolls[i];
-            }
-        };
-
-        //console.log(tmp);
-        if(tmp >= 10){
-            ++mise;
-        }
-
-        return mise;
-    }
     
     rerollDice(){
         for(const arg in arguments){
@@ -84,6 +33,7 @@ module.exports = class Roll{
             if(parseInt(arguments[arg])>this.datas.length){return 0}
             else{this.datas[parseInt(arguments[arg])-1] = Math.floor(Math.random() * Math.floor(this.typeOfDice))+1;}
         }
+        sortNumbers(this.datas)
         return 1;
     }
 }
